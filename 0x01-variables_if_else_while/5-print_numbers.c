6238feead5d48d72d1d7cc2ddd9d1e5fb265d688
Moyahabo Hamese <habohamese@gmail.com>
2023-02-20 07:50:45 +0200

    This program prints numbers using printf and putchar

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - For loop
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - For loop
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
