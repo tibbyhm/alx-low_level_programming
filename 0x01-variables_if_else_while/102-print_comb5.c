80adcb150113be8665374bdfb2d33f0170d3ff5e
Moyahabo Hamese <habohamese@gmail.com>
2023-02-20 10:41:12 +0200

    This program prints all combinations of 2 2-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -15,10 +14,10 @@ int main(void)
 		for (j = i + 1; j <= 99; j++)
 		{
 		putchar((i / 10) + '0');
-		putchar((j % 10) + '0');
+		putchar((i % 10) + '0');
 		putchar(' ');
 
-		putchar((i / 10) + '0');
+		putchar((j / 10) + '0');
 		putchar((j % 10) + '0');
 
 		if (i == 98 && j == 99)
