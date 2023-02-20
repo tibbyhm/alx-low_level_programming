92ed7f97d1929c50899da9dd6941d87ffa7a6fcc
Moyahabo Hamese <habohamese@gmail.com>
2023-02-20 10:00:03 +0200

    This program shows all combinations of 2 digits with no repetitions

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - For loop
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+		putchar((i % 10) + '0');
+		putchar((j % 10) + '0');
+
+		if (i == 8 && j == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
