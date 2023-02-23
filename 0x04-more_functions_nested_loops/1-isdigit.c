d9d27a4c6b6d18f94fcaa969e545c0cfbdffe5c8
Moyahabo Hamese <habohamese@gmail.com>
2023-02-23 14:47:07 +0200

    This program prints 1 if the input parameter is a digit and 0 if otherwise

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for input parameter is a digit.
+ *
+ * @c: Input parameter that is being checked
+ *
+ * Return: 1 if uppercase
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
