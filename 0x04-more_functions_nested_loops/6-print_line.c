40c3702c81b2e0a0770ec3810f33e869a3bdd928
Moyahabo Hamese <habohamese@gmail.com>
2023-02-24 14:37:09 +0200

    This program prints _ n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_line - Prints _ n times
+* @n: Input parameter
+*
+* Return: Void
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+			_putchar('_');
+	}
+	_putchar('\n');
+}
