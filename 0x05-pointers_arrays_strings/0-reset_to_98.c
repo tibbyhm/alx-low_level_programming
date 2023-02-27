e497c4f5698259245b0f79364bccd553ab387924
Moyahabo Hamese <habohamese@gmail.com>
2023-02-27 08:46:08 +0200

    This programs allocates a variable the value 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Points input variable to reset_to_98
+ *
+ * @n: Pointer input variable
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
