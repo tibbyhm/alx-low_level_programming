c6c71f48601ac82c0753d911da3edcd8cb9cbf1b
Moyahabo Hamese <habohamese@gmail.com>
2023-02-28 14:53:59 +0200

    This function imitates strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strcpy - Copies a string from source to destination
+*
+* @dest: Where string is copied to
+* @src: Where the string is copied from
+*
+* Return: Pointer to destination
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+		while (*src != '\0')
+		{
+			*dest = *src;
+			src++;
+			dest++;
+		}
+		*dest = '\0';
+		return (dest);
+}
