221ac3c03957cb81376b388ba71e1550bcb4e860
Moyahabo Hamese <habohamese@gmail.com>
2023-03-02 15:35:03 +0200

    This program converts a string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
-
+#include <ctype.h>
 /**
-* stri
+* string_toupper - Converts a string to uppercase
+* @arr: The string that will be converttes to uppercase
+*
+* Return: Pointer to char
+*/
+
+char *string_toupper(char *arr)
+{
+	int i;
+
+	for (i = 0; arr[i]; i++)
+	{
+		arr[i] = (char)toupper(arr[i]);
+	}
+	return (arr);
+}
