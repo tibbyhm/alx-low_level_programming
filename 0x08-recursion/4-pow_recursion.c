1c07fbe4efd284bf70704907c00b5aa72a1a0150
Moyahabo Hamese <tibbyhm@gmail.com>
2023-03-08 12:15:36 +0200

    This program finds x^y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - Finding x to the power of y
+ *
+ * @x: The base
+ * @y: The power
+ *
+ * Return: x to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	else if (y == 0)
+		return (1);
+
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
